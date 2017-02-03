cc9183393313ff0d557366362a819f850467003d
Matthew Li <mdeng1985@gmail.com>
2017-02-03 09:27:12 -0600

    update descrpitionTest.m to test description, parameters and return
    Signed-off-by: Matthew Li <mdeng1985@gmail.com>

---
+++ src/descriptionTest.m
@@ -0,0 +1,17 @@
+function y=descriptionTest(a)
+% This function tests comments of decription,parameters, return values (brief description)
+%
+% This is detailed description (After the first empty documentation line, paragraphs of the detailed
+% description begin)
+%
+% see: http://www.stack.nl/~dimitri/doxygen/manual/docblocks.html#docstructure
+%
+% also see: http://mdengli.com/mtocppLearn/ for a detail explaination
+%
+% Parameters:
+%  a: first parameter of type testnamespace.testclass.
+%
+% Return values:
+%  y: return value
+
+y=a;
---
+++ src/doxygen1.m
@@ -0,0 +1,17 @@
+function rv = doxygen1(param1, param2, auto_param)
+% Here comes a short description text
+%
+% After the first empty documentation line, paragraphs of the detailed
+% description begin.
+% 
+% Lines beginning with the words "Parameters" or "Return values" start a block
+% of argument respectively return argument descriptions.
+%
+% Parameters:
+%  param1: first parameter of type testnamespace.testclass.
+%  auto_param: of type ::gridbase
+%
+% Return values:
+%  rv: return value
+%
+
